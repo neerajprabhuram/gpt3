1067c9afc8c1a982cae070904583a9b0716c5955
Neeraj <nxxwxj@gmail.com>
2022-01-11 17:07:07 +0530

    completed cta

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,16 @@ import './cta.css';
 
 const CTA = () => {
     return (
-        <div>
-            CTA
+        <div className='margin'>
+            <div className='gpt3__cta section__margin'>
+                <div className='gpt3__cta-content'>
+                    <p>Request early access</p>
+                    <h3><h3>Register Today & start exploring the endless possibilities.</h3></h3>
+                </div>
+                <div className='gpt3__cta-button'>
+                    <button type='button'>Get started</button>
+                </div>
+            </div>
         </div>
     )
 }
